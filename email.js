7fd9b441bb8d830271b5d72e5395b4c08155aaa1
dheeraj <dheeraj.rajaram@taodigitalusa.com>
2023-11-13 09:05:30 +0530

    save progress

--- email.js
+++ email.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 export const transporter = nodemailer.createTransport({
     service: "gmail",
     auth:{
-        user: "lockedandloaded99@gmail.com",
-        pass: "jtbwoovgyhfchojj"
+        user: "ragavdheeraj@gmail.com",
+        pass: "lgohmkbzbjkntptn"
     }
 });
